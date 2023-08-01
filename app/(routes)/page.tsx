1dd1f4acbbc5c0d748cba9e5a307ab645065fdf9
Rafi Shaik <rafishaik8208@gmail.com>
2023-08-01 10:12:23 +0530

    initial commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,14 @@
+import Billboard from "@/components/Billboard"
+import Container from "@/components/ui/Container"
+
+const HomePage = () => {
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+<Billboard/>
+      </div>
+    </Container>
+  )
+}
+
+export default HomePage
